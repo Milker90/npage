115e67b7079b201523eed745a58973b00cd8fa8d
tommy <liuzhihai@fangfuwang.com>
2020-04-04 10:46:29 +0800

    add viewModel isDisposed

--- lib/src/view_model.dart
+++ lib/src/view_model.dart
@@ -2,8 +2,15 @@ import 'package:flutter/material.dart';
 import 'npage_view_state.dart';
 
 class ViewModel extends ChangeNotifier {
+  bool isDisposed = false;
   final NPageViewState pageViewState;
   ViewModel( 
     {pageViewStateValue : NPageViewStateValue.Init})
     : pageViewState = NPageViewState(pageViewStateValue); 
+
+  @override
+  void dispose() {
+    isDisposed = true;
+    super.dispose();
+  }
 }
